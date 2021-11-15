c34f890306b70d19636e6a9e853389cd113dfedd
celestroccayoli <celestetroccayoli@gmail.com>
2021-11-15 19:37:56 +0100

    Estilos

--- app.js
+++ app.js
@@ -1,67 +1,23 @@
-const express = require("express");
+const express = require ("express");
+const path = require ("path");
 const app = express();
-const path = require("path");
-const miRuta =path.resolve(__dirname, "./public");
+const publicPath = path.resolve( __dirname, "./public");
 
-app.use (express.static(miRuta));
+app.use ( express.static(publicPath) );
+ 
+app.listen(process.env.PORT || 3000, () => {console.log("Servidor corriendo en el puerto 3000")});
 
-app.listen(process.env.PORT || 3000, () => { console.log("Servidor corriendo en el puerto 3000"); });
-
-app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/home.html"))
-});
-
-app.post("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/home.html"))
-});
-
-app.get("/register", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/registro.html"))
+app.get("/", (req , res) => {
+    res.sendFile( path.resolve (__dirname, "./views/home.html"))
 })
 
-app.get("/detalle-de-productos", (req, res) =>{
-    res.sendFile(path.resolve(__dirname, "./views/detalle-de-productos.html"))
-});
-app.get("/login", (req, res) =>{
-    res.sendFile(path.resolve(__dirname, "./views/login.html"))
-});
-app.get("/clase15", (req, res) =>{
-    res.sendFile(path.resolve(__dirname, "./views/formulario-clase15.html"))
-});
-app.get("/carrito-de-compras", (req, res) =>{
-    res.sendFile(path.resolve(__dirname, "./views/carrito-de-compras.html"))
-});
-app.post("/carrito-de-compras", (req, res) =>{
-    res.sendFile(path.resolve(__dirname, "./views/carrito-de-compras.html"))
-});
-app.get("/productos", (req, res) =>{
-    res.sendFile(path.resolve(__dirname, "./views/listado-de-productos.html"))
-});
-app.get("/contacto", (req, res) =>{
-    res.sendFile(path.resolve(__dirname, "./views/contacto.html"))
-});
-app.get("/nosotros", (req, res) =>{
-    res.sendFile(path.resolve(__dirname, "./views/nosotros.html"))
-});
-app.get("/terminos-y-condiciones", (req, res) =>{
-    res.sendFile(path.resolve(__dirname, "./views/terminos-y-condiciones.html"))
-});
-app.get("/novedades", (req, res) =>{
-    res.sendFile(path.resolve(__dirname, "./views/novelties.html"))
-});
-app.get("/recetas", (req, res) =>{
-    res.sendFile(path.resolve(__dirname, "./views/recipes.html"))
-});
-app.get("/perfil", (req, res) =>{
-    res.sendFile(path.resolve(__dirname, "./views/profile.html"))
-});
-app.post("/perfil", (req, res) =>{
-    res.sendFile(path.resolve(__dirname, "./views/profile.html"))
-});
-app.get("/registro", (req, res) =>{
-    res.sendFile(path.resolve(__dirname, "./views/registro.html"))
-});
-app.get("/metodos-de-pago", (req, res) =>{
-    res.sendFile(path.resolve(__dirname, "./views/metodos-de-pago.html"))
-});
+app.post("/", (req , res) => {
+    res.sendFile( path.resolve (__dirname, "./views/home.html"))
+})
 
+app.get("/login", (req , res) => {
+    res.sendFile( path.resolve (__dirname, "./views/login.html"))
+});
+app.get("/register", (req , res) => {
+    res.sendFile( path.resolve (__dirname, "./views/register.html"))
+})
